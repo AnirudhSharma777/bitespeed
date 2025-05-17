da89efc651a831b1bea4c147d6c8493145de7044
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-17 19:26:26 +0530

    Exceptions

---
+++ src/main/java/com/bitespeed/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.bitespeed.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.bitespeed.ResponseDto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+   // Fallback for unexpected errors
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception ex) {
+        return new ResponseEntity<>(new ErrorResponse("INTERNAL_SERVER_ERROR", ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
