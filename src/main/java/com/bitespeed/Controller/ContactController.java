0a338b7c68af5332e93d69b79d829149e75a004e
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-17 19:12:52 +0530

    controller

---
+++ src/main/java/com/bitespeed/Controller/ContactController.java
@@ -0,0 +1,27 @@
+package com.bitespeed.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitespeed.Dto.IdentifyRequest;
+import com.bitespeed.ResponseDto.IdentifyResponseDto;
+import com.bitespeed.Services.ContactServiceImpl;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final ContactServiceImpl contactService;
+
+    @PostMapping("/identify")
+    public ResponseEntity<IdentifyResponseDto> identify(@Valid @RequestBody IdentifyRequest request) throws Exception{
+        return ResponseEntity.ok(contactService.identifyCustomer(request));
+    }
+}
