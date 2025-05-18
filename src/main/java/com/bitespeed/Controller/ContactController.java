788a14234ff66b1b40687b31d1610710b1a3e60a
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-18 07:46:31 +0530

    add GetMapping

--- src/main/java/com/bitespeed/Controller/ContactController.java
+++ src/main/java/com/bitespeed/Controller/ContactController.java
@@ -1,6 +1,9 @@
 package com.bitespeed.Controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,10 @@ public class ContactController {
     @PostMapping("/identify")
     public ResponseEntity<IdentifyResponseDto> identify(@Valid @RequestBody IdentifyRequest request) throws Exception{
         return ResponseEntity.ok(contactService.identifyCustomer(request));
+    }
+
+    @GetMapping("/identify")
+    public ResponseEntity<List<IdentifyResponseDto>> getIdentify() throws Exception{
+        return ResponseEntity.ok(contactService.getAllIdentifyCustomer());
     }
 }
