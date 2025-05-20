1c7a2a876427ce5aa6bb7c1c269494fed713df5c
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-20 08:25:05 +0530

    IdentifyRequest

--- src/main/java/com/bitespeed/Dto/IdentifyRequest.java
+++ src/main/java/com/bitespeed/Dto/IdentifyRequest.java
@@ -1,12 +1,13 @@
 package com.bitespeed.Dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 
 public record IdentifyRequest(
 
     @Email(message = "Invalid email format")
     String email,
 
-    
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     String phoneNumber
 ) {}
